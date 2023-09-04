fa69e1d986fd615291ce1feac5d34a220286ad80
Gagan Shrestha <g.shrestha.13@student.scu.edu.au>
2023-09-04 18:28:27 +0545

    Added Functional requirement 2 code: Input students marks,accept valid mark

--- Assignment 1/StudentStatistics.java
+++ Assignment 1/StudentStatistics.java
@@ -7,15 +7,39 @@
  * @version (04/09/2023)
  */
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class StudentStatistics{
     public static void main(String[] args){
         // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
+        List<Integer> studentMarks = new ArrayList<>();
+        
         // Functional Requirement 1 (F1): Input Assignment Name
-        //Step 1: Display a prompt to the user to input the assignment name
+        // Display a prompt to the user to input the assignment name
         System.out.print("Enter the assignment name: ");
-        //Step 2: Read and store the assignment name input from the user.
+        // Read and store the assignment name input from the user.
         String assignmentName = scanner.nextLine();
+        
+        //Functional Requirement 2 (F2): Input Students' Marks
+        //Start loop for students
+        for(int i=0;i<30;i++){
+            int mark;
+            do{
+                //Display a prompt to the user to input the mark for the current student.
+                System.out.print("Enter mark for student " + (i+1)+": ");
+                
+                //Read and validate the input mark
+                while (!scanner.hasNextInt()){
+                    System.out.println("Invalid input. Please enetr a number.");
+                    scanner.next();//Consume the invalid input
+                }
+                mark = scanner.nextInt();
+                //Check if the input is a valid number between 0 and 30. 
+            }while (mark<0 || mark>30);
+            //Add the valid mark to the list of marks.
+            studentMarks.add(mark);
+        }
         //Display the assignment name.
         System.out.println("Assignment Name: " + assignmentName);
         
